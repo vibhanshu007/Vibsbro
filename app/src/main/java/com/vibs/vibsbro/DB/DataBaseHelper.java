34e5040abd7c386a5f0e61d06a1600ea53f2f7fb
vibhanshu007 <vibhanshumail849@gmail.com>
2017-05-15 18:44:40 +0530

    Login and Signup complete

---
+++ app/src/main/java/com/vibs/vibsbro/DB/DataBaseHelper.java
@@ -0,0 +1,158 @@
+package com.vibs.vibsbro.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.vibs.vibsbro.User;
+
+/**
+ * Created by root on 15/5/17.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "UserManager.db";
+
+    // User table name
+    private static final String TABLE_USER = "user";
+
+    // User Table Columns names
+    private static final String COLUMN_USER_ID = "user_id";
+    private static final String COLUMN_USER_NAME = "user_name";
+    private static final String COLUMN_USER_EMAIL = "user_email";
+    private static final String COLUMN_USER_PASSWORD = "user_password";
+
+    // create table sql query
+    private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
+            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
+            + COLUMN_USER_EMAIL + " TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ")";
+
+    // drop table sql query
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER;
+
+    public DataBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //Drop User Table if exist
+        sqLiteDatabase.execSQL(DROP_USER_TABLE);
+
+        // Create tables again
+        onCreate(sqLiteDatabase);
+
+    }
+    public void addUser(User user){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_USER_NAME,user.getName());
+        contentValues.put(COLUMN_USER_EMAIL,user.getEmail());
+        contentValues.put(COLUMN_USER_PASSWORD,user.getPassword());
+        // Inserting Row
+        sqLiteDatabase.insert(TABLE_USER,null,contentValues);
+        sqLiteDatabase.close();
+
+    }
+    public void updateUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USER_NAME, user.getName());
+        values.put(COLUMN_USER_EMAIL, user.getEmail());
+        values.put(COLUMN_USER_PASSWORD, user.getPassword());
+
+        // updating row
+        db.update(TABLE_USER, values, COLUMN_USER_ID + " = ?",
+                new String[]{String.valueOf(user.getId())});
+        db.close();
+    }
+
+    public boolean checkUser(String email) {
+
+        // array of columns to fetch
+        String[] columns = {
+                COLUMN_USER_ID
+        };
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // selection criteria
+        String selection = COLUMN_USER_EMAIL + " = ?";
+
+        // selection argument
+        String[] selectionArgs = {email};
+
+        // query user table with condition
+        /**
+         * Here query function is used to fetch records from user table this function works like we use sql query.
+         * SQL query equivalent to this query function is
+         * SELECT user_id FROM user WHERE user_email = 'vibsbro@gmail.com';
+         */
+        Cursor cursor = db.query(TABLE_USER, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                      //filter by row groups
+                null);                      //The sort order
+        int cursorCount = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        if (cursorCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkUser(String email, String password) {
+
+        // array of columns to fetch
+        String[] columns = {
+                COLUMN_USER_ID
+        };
+        SQLiteDatabase db = this.getReadableDatabase();
+        // selection criteria
+        String selection = COLUMN_USER_EMAIL + " = ?" + " AND " + COLUMN_USER_PASSWORD + " = ?";
+
+        // selection arguments
+        String[] selectionArgs = {email, password};
+
+        // query user table with conditions
+        /**
+         * Here query function is used to fetch records from user table this function works like we use sql query.
+         * SQL query equivalent to this query function is
+         * SELECT user_id FROM user WHERE user_email = 'vibsbro@gmail.com' AND user_password = 'vibs1234';
+         */
+        Cursor cursor = db.query(TABLE_USER, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                       //filter by row groups
+                null);                      //The sort order
+
+        int cursorCount = cursor.getCount();
+
+        cursor.close();
+        db.close();
+        if (cursorCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
--- app/src/main/java/com/vibs/vibsbro/MainActivity.java
+++ app/src/main/java/com/vibs/vibsbro/MainActivity.java
@@ -1,19 +1,16 @@
 package com.vibs.vibsbro;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -22,19 +19,25 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.squareup.picasso.Picasso;
+import com.vibs.vibsbro.DB.DataBaseHelper;
+import com.vibs.vibsbro.DB.InputValidation;
 
-import static com.facebook.share.internal.GameRequestValidation.validate;
 
+public class MainActivity extends Activity implements View.OnClickListener {
 
-public class MainActivity extends Activity {
-    LoginButton fbLoginButton;
-    TextView loginTextview;
+    private NestedScrollView nestedScrollView;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private AppCompatButton appCompatButtonLogin;
+    private AppCompatTextView textViewLinkRegister;
+    private AppCompatImageView imageViewLogo;
+    private InputValidation inputValidation;
+    private DataBaseHelper databaseHelper;
     CallbackManager callbackManager;
-    ImageView logo;
-    EditText username_edittext,password_edittext;
-    Button loginButton;
-    TextView singUpLink,forgot_password;
-    RelativeLayout relativeLayout;
+    LoginButton fbLoginButton;
+
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
@@ -42,40 +45,119 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(this.getApplicationContext());
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        //getID section
-        relativeLayout = (RelativeLayout)findViewById(R.id.login_parent);
-        fbLoginButton = (LoginButton)findViewById(R.id.login_button);
-        logo = (ImageView)findViewById(R.id.logo);
-        loginButton = (Button)findViewById(R.id.login_button);
-        username_edittext = (EditText)findViewById(R.id.username);
-        password_edittext = (EditText)findViewById(R.id.password);
-        singUpLink = (TextView)findViewById(R.id.signup_id);
-        forgot_password = (TextView)findViewById(R.id.forgot_password_id);
+        initViews();
 
         //Picasso section
         Picasso
                 .with(this)
                 .load(R.drawable.logo)
                 .resize(150,200)
-                .into(logo);
+                .into(imageViewLogo);
 
-        //Facebook sevtion
+        initListner();
+        initObjects();
+        fbIntegration(); //facebook integration
 
+    }
+
+    //init workes...
+    private void initViews(){
+
+        imageViewLogo = (AppCompatImageView) findViewById(R.id.logo);
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+        appCompatButtonLogin = (AppCompatButton) findViewById(R.id.appCompatButtonLogin);
+        textViewLinkRegister = (AppCompatTextView) findViewById(R.id.textViewLinkRegister);
+        fbLoginButton = (LoginButton)findViewById(R.id.login_button);
+    }
+
+    private void initListner(){
+        appCompatButtonLogin.setOnClickListener(this);
+        textViewLinkRegister.setOnClickListener(this);
+    }
+
+    private void initObjects() {
+        databaseHelper = new DataBaseHelper(MainActivity.this);
+        inputValidation = new InputValidation(MainActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.appCompatButtonLogin:
+                verifyFromSQLite();
+                break;
+            case R.id.textViewLinkRegister:
+                // Navigate to RegisterActivity
+                Intent intentRegister = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intentRegister);
+                break;
+        }
+
+    }
+    /**
+     * This method is to validate the input text fields and verify login credentials from SQLite
+     */
+    private void verifyFromSQLite() {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail,
+                getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail,
+                getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword,
+                getString(R.string.error_message_email))) {
+            return;
+        }
+
+        if (databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim()
+                , textInputEditTextPassword.getText().toString().trim())) {
+
+
+            Intent accountsIntent = new Intent(MainActivity.this, HomeActivity.class);
+            accountsIntent.putExtra("EMAIL", textInputEditTextEmail.getText().toString().trim());
+            emptyInputEditText();
+            startActivity(accountsIntent);
+
+
+        } else {
+            // Snack Bar to show success message that record is wrong
+            Snackbar.make(nestedScrollView, getString(R.string.error_valid_email_password),
+                    Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private void emptyInputEditText() {
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+    }
+
+    private void fbIntegration(){
         fbLoginButton.setReadPermissions("user_friends");
         callbackManager = CallbackManager.Factory.create();
         fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                loginTextview.setText("Login Successfully \n" +loginResult.getAccessToken()
-                        .getUserId()+"\n"+loginResult.getAccessToken().getToken());
+                /*loginTextview.setText("Login Successfully \n" +loginResult.getAccessToken()
+                        .getUserId()+"\n"+loginResult.getAccessToken().getToken());*/
 
             }
 
             @Override
             public void onCancel() {
-                loginTextview.setText("Login Cancel");
+               // loginTextview.setText("Login Cancel");
 
             }
 
@@ -84,102 +166,6 @@ public class MainActivity extends Activity {
 
             }
         });
-
-        //ClickEvent Setions
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
-
-        singUpLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        forgot_password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    //ToDo work on login method
-    public void login(){
-        Log.d(TAG, "Login");
-
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-        loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this,
-                R.style.Theme_AppCompat_DayNight);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-        String username = username_edittext.getText().toString();
-        String password = password_edittext.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String email = username_edittext.getText().toString();
-        String password = password_edittext.getText().toString();
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            username_edittext.setError("enter a valid email address");
-            valid = false;
-        } else {
-            username_edittext.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            password_edittext.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            password_edittext.setError(null);
-        }
-
-        return valid;
-    }
-
-    public void onLoginSuccess() {
-        loginButton.setEnabled(true);
-        finish();
-    }
-
-    public void onLoginFailed() {
-        Snackbar.make(relativeLayout,"Login Fail",Snackbar.LENGTH_LONG);
-        loginButton.setEnabled(true);
-    }
-
-
 
 }
